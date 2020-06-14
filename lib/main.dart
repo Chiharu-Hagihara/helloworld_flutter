33ea9742fbe345b2e3a6507f318b716fed497467
M-E-C <Mr_El_Capitan@outlook.jp>
2020-06-14 15:53:30 +0900

    ステートの操作

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,42 @@ import 'package:flutter/material.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
+  final title = 'FlutterSample';
+  final message = 'Sample Message';
 
+  @override
+  Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello Flutter!'),
-        ),
+      home: new MyHomePage(
+          title: this.title,
+          message: this.message
+      ),
+    );
+  }
+}
 
-        body: Text(
-        'Hello, Flutter World!!!',
+class MyHomePage extends StatefulWidget {
+  final String title;
+  final String message;
+
+  MyHomePage({this.title, this.message}): super();
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Text(
+        widget.message,
         style: TextStyle(fontSize: 32.0),
-        ),
       ),
     );
   }
