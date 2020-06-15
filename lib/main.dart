03b5fe34b8fb4011be3703a22bdd2612348c5122
M-E-C <Mr_El_Capitan@outlook.jp>
2020-06-15 18:35:00 +0900

    Click Counter

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,20 @@ import 'package:flutter/material.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  final title = 'FlutterSample';
-
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Flutter Demo',
-      home: new MyHomePage(
-          title: this.title,
+      theme: new ThemeData(
+        primarySwatch: Colors.blue
       ),
+      home: new MyHomePage(title: 'Flutter Sample App'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({this.title}): super();
-
+  MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
 
   @override
@@ -26,33 +24,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _message;
+  int _counter = 0;
 
-  @override
-  void initState() {
-    super.initState();
-    _message = 'Hello!';
-  }
-
-  void _setMessage(){
+  void _incrementCounter() {
     setState(() {
-      _message = 'tapped!';
+      _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Text(
-        _message,
-        style: TextStyle(fontSize: 32.0),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: <Widget>[
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            )
+          ],
+        ),
       ),
+
       floatingActionButton: FloatingActionButton(
-        onPressed: _setMessage,
+        onPressed: _incrementCounter,
         tooltip: 'set message.',
         child: Icon(Icons.star),
       ),
